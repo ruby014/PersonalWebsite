203101e282a95854aa33c9ccf0455d565774e49d
Ruchelle <rbaybayan2@myseneca.ca>
2025-04-22 14:43:09 -0400

    completed Header

---
+++ portfolio/app/components/Header.jsx
@@ -0,0 +1,34 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
+        <div>
+            <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
+        </div>
+        <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>
+            Hi, I'm Ruchelle! <Image src={assets.hand_icon} alt='' className='w-6'/>
+        </h3>
+        <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-ovo'>
+            computer programming student based in Toronto.
+        </h1>
+        <p className='max-w-2xl text-base mx-auto font-Ovo'>
+            Carrying a unique blend of experience from healthcare to tech — I enjoy building impactful and user-focused software.
+        </p>
+        <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+            <a href='#contact' className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 font-outfit text-base'>
+                contact me
+                <Image src={assets.right_arrow_white} alt='' className='w-4'/>
+            </a>
+            <a href='/ruchelle_baybayan_resume.pdf' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 font-outfit text-base'>
+                my resume
+                <Image src={assets.download_icon} alt='' className='w-4'/>
+            </a>
+        </div>
+    </div>
+  )
+}
+
+export default Header
