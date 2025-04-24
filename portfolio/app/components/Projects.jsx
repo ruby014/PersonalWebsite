83a4727f38a0d2215f1ee3d5afcf68d13fe5df64
Ruchelle <rbaybayan2@myseneca.ca>
2025-04-23 23:26:04 -0400

    modified card style

--- portfolio/app/components/Projects.jsx
+++ portfolio/app/components/Projects.jsx
@@ -12,23 +12,22 @@ const Projects = () => {
         </p>
 
         {/** container for the projects */}
-        <div className='grid grid-cols-auto my-10 gap-5 '>
+        <div className='grid grid-cols-2 my-10 gap-5 '>
             {/** project #1 */}
             {projectData.map((project, index) => (
                 <div key={index} 
                 className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative 
-                cursor-pointer group grayscale-75 hover:grayscale-0 transition duration-500'
+                group grayscale-75 border-gray-200 hover:grayscale-0 transition duration-300 hover:shadow-black'
                 style={{backgroundImage: `url(${project.bgImage})`}}>
-                    <div className='bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5
-                     flex items-center justify-between duration-500 group-hover:bottom-7'>
+                    <div className='h-52 md:h-80 bg-gray-400 w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5
+                     flex items-center justify-between duration-500 group-hover:bottom-7 hover:bg-lightHover'></div>
                         <div>
                             <h2 className='font-outfit'>{project.title}</h2>   
-                            <p className='font-outfit text-sm'>{project.description}</p>
+                            {/* <p className='font-outfit text-sm'>{project.description}</p> */}
                         </div>
                         <div>
                             <Image src={assets.send_icon} alt='send icon' className='w-5'/>
                         </div>
-                    </div>
                 </div>
             ))}
 
