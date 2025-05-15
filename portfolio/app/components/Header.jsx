7041a464388a76f770eb33c3114d18ecb424d0d0
Ruchelle <rbaybayan2@myseneca.ca>
2025-05-15 19:59:35 -0400

    fixed spacing with image from navbar

--- portfolio/app/components/Header.jsx
+++ portfolio/app/components/Header.jsx
@@ -7,6 +7,8 @@ const Header = () => {
   return (
     <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
         <div>
+            <br></br>
+            <br></br>
             <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
         </div>
         <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>
