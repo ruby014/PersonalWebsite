bbc5040afe3ac0d9607f855dcc0fea3c76b02d0b
Ruchelle <rbaybayan2@myseneca.ca>
2025-05-17 17:42:45 -0400

    changed back layout and page.js

--- app/layout.js
+++ app/layout.js
@@ -1,32 +1,28 @@
-// import { Outfit, Ovo } from "next/font/google";
-// import "./globals.css";
+import { Outfit, Ovo } from "next/font/google";
+import "./globals.css";
 
-// const outfit = Outfit({
-//   subsets: ["latin"], weight: ["400", "500", "600", "700"]
-// });
+const outfit = Outfit({
+  subsets: ["latin"], weight: ["400", "500", "600", "700"]
+});
 
-// const ovo = Ovo({
-//   subsets: ["latin"], weight: ["400"]
-// });
+const ovo = Ovo({
+  subsets: ["latin"], weight: ["400"]
+});
 
-// export const metadata = {
-//   title: "Ruchelle Baybayan",
-//   description: "A website to display my portfolio",
-// };
-
-// export default function RootLayout({ children }) {
-//   return (
-//     <html lang="en" className="scroll-smooth">
-//       <body
-//         className={`${outfit.className} ${ovo.className} antialiased leading-8
-//         overflow-x-hidden`}
-//       >
-//         {children}
-//       </body>
-//     </html>
-//   );
-// }
+export const metadata = {
+  title: "Ruchelle Baybayan",
+  description: "A website to display my portfolio",
+};
 
 export default function RootLayout({ children }) {
-  return <html><body>{children}</body></html>;
+  return (
+    <html lang="en" className="scroll-smooth">
+      <body
+        className={`${outfit.className} ${ovo.className} antialiased leading-8
+        overflow-x-hidden`}
+      >
+        {children}
+      </body>
+    </html>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,26 +1,22 @@
-// 'use client'
-// import About from "./About";
-// import EasterEgg from "./EasterEgg";
-// import Header from "./Header";
-// import Navbar from "./Navbar";
-// import Projects from "./Projects";
-// import Tools from "./Tools";
-// import Work from "./Work";
-
-// export default function Home() {
-//   return (
-//     <>
-//     <Navbar />
-//     <Header />
-//     <About />
-//     <Tools />
-//     <Work />
-//     <Projects />
-//     <EasterEgg />
-//     </>
-//   );
-// }
+'use client'
+import About from "./About";
+import EasterEgg from "./EasterEgg";
+import Header from "./Header";
+import Navbar from "./Navbar";
+import Projects from "./Projects";
+import Tools from "./Tools";
+import Work from "./Work";
 
 export default function Home() {
-  return <h1>Hello, world!</h1>;
+  return (
+    <>
+    <Navbar />
+    <Header />
+    <About />
+    <Tools />
+    <Work />
+    <Projects />
+    <EasterEgg />
+    </>
+  );
 }
