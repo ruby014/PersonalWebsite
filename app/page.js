48a98d33f774d19209c036f86fdfae2ca6401d78
Ruchelle <rbaybayan2@myseneca.ca>
2025-05-17 17:36:47 -0400

    testing deployment with minimal page.js

--- app/page.js
+++ app/page.js
@@ -1,22 +1,26 @@
-'use client'
-import About from "./About";
-import EasterEgg from "./EasterEgg";
-import Header from "./Header";
-import Navbar from "./Navbar";
-import Projects from "./Projects";
-import Tools from "./Tools";
-import Work from "./Work";
+// 'use client'
+// import About from "./About";
+// import EasterEgg from "./EasterEgg";
+// import Header from "./Header";
+// import Navbar from "./Navbar";
+// import Projects from "./Projects";
+// import Tools from "./Tools";
+// import Work from "./Work";
+
+// export default function Home() {
+//   return (
+//     <>
+//     <Navbar />
+//     <Header />
+//     <About />
+//     <Tools />
+//     <Work />
+//     <Projects />
+//     <EasterEgg />
+//     </>
+//   );
+// }
 
 export default function Home() {
-  return (
-    <>
-    <Navbar />
-    <Header />
-    <About />
-    <Tools />
-    <Work />
-    <Projects />
-    <EasterEgg />
-    </>
-  );
+  return <h1>Hello, world!</h1>;
 }
